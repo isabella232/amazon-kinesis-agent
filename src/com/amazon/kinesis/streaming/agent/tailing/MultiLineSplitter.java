9e3450c228316feb0438223666b5abe7ae1a4326
Matthew Leventi <mleventi@lyft.com>
2015-12-17 13:23:28 -0800

    group multiple lines into a record on split

--- src/com/amazon/kinesis/streaming/agent/tailing/MultiLineSplitter.java
+++ src/com/amazon/kinesis/streaming/agent/tailing/MultiLineSplitter.java
@@ -1,6 +1,7 @@
 package com.amazon.kinesis.streaming.agent.tailing;
 
 import java.nio.ByteBuffer;
+import org.slf4j.Logger;
 
 import com.amazon.kinesis.streaming.agent.ByteBuffers;
 
@@ -13,6 +14,7 @@ import com.amazon.kinesis.streaming.agent.ByteBuffers;
  */
 public class MultiLineSplitter implements ISplitter {
     public static final char LINE_DELIMITER = '\n';
+    private static final Logger LOGGER = Logging.getLogger(MultiLineSplitter.class);
     private final int maxLinesPerRecord;
 
     public MultiLineSplitter(int maxLinesPerRecord) {
@@ -32,19 +34,24 @@ public class MultiLineSplitter implements ISplitter {
               return -1;
             } else {
               buffer.position(lastPosition);
-              return lastPosition;
+              break;
             }
           }
           int length = currentPosition - startPosition;
           if (length >= 1000000) {
             if (lastPosition == -1) {
-              return currentPosition;
+              lastPosition = currentPosition
+              break;
             } else {
               buffer.position(lastPosition);
-              return lastPosition;
+              break;
             }
           }
           lastPosition = currentPosition;
+        }
+        if (lastPosition != -1) {
+          int length = currentPosition - startPosition;
+          LOGGER.debug("Created Record Size: {}", length);
         }
         return lastPosition;
     }

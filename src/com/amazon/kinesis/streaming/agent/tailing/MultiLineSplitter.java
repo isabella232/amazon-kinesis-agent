8e30c0eb1e5d782160f16587ccd351c3cd1a14f8
Matthew Leventi <mleventi@lyft.com>
2015-12-17 12:59:50 -0800

    group multiple lines into a record on split

---
+++ src/com/amazon/kinesis/streaming/agent/tailing/MultiLineSplitter.java
@@ -0,0 +1,52 @@
+package com.amazon.kinesis.streaming.agent.tailing;
+
+import java.nio.ByteBuffer;
+
+import com.amazon.kinesis.streaming.agent.ByteBuffers;
+
+/**
+ * Returns a record for several lines.
+ * TODO: Limitation of this splitter is that it requires a newline at the end
+ *       of the file, otherwise it will miss the last record. We should be able
+ *       to handle this at the level of the {@link IParser} implementation.
+ * TODO: Should we parametrize the line delimiter?
+ */
+public class MultiLineSplitter implements ISplitter {
+    public static final char LINE_DELIMITER = '\n';
+    private int final maxLinesPerRecord;
+
+    public MultiLineSplitter(int maxLinesPerRecord) {
+      this.maxLinesPerRecord = maxLinesPerRecord;
+    }
+
+    @Override
+    public int locateNextRecord(ByteBuffer buffer) {
+        // TODO: Skip empty records, commented records, header lines, etc...
+        //       (based on FileFlow configuration?).
+        int startPosition = buffer.position();
+        int lastPosition = -1;
+        for (int i=0; i < maxLinesPerRecord; i++) {
+          int currentPosition = ByteBuffers.advanceBufferToNextLine(buffer);
+          if (currentPosition == -1) {
+            if (lastPosition == -1) {
+              return -1;
+            } else {
+              buffer.position(lastPosition);
+              return lastPosition;
+            }
+          }
+          int length = currentPosition - startPosition;
+          if (length >= 1000000) {
+            if (lastPosition == -1) {
+              return currentPosition;
+            } else {
+              buffer.position(lastPosition)
+              return lastPosition;
+            }
+          }
+          lastPosition = currentPosition;
+        }
+        return lastPosition;
+      }
+    }
+}
